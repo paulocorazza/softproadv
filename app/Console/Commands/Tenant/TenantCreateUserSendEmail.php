f9449a9b886b09c124f6e4727147259d9d7c11a9
Carlos Junior <carlos@theplace.com.br>
2021-07-30 16:16:36 -0300

    ajuste http para enviar email

--- app/Console/Commands/Tenant/TenantCreateUserSendEmail.php
+++ app/Console/Commands/Tenant/TenantCreateUserSendEmail.php
@@ -83,9 +83,21 @@ class TenantCreateUserSendEmail extends Command
 
     private function hasDNSAvailable(Company $company)
     {
-        $response = Http::get( $company->subdomain . config('tenant.subdomain'));
+        return $this->getUrlStatus($company->subdomain . config('tenant.subdomain'));
 
-        return $response->ok();
+    }
+
+    private function getUrlStatus($url)
+    {
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 5);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $data = curl_exec($ch);
+        $httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        curl_close($ch);
+
+        return $httpcode >= 200 && $httpcode < 300;
     }
 
 
