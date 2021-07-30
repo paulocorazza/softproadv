fc2122f73dab2b70f877247f0cd5f2f0e522793c
Carlos Junior <carlos@theplace.com.br>
2021-07-30 18:32:12 -0300

    force https no app service provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -29,7 +29,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        URL::forceScheme('https');
+        //URL::forceScheme('https');
 
         Schema::defaultStringLength('191');
         Paginator::useBootstrap();
