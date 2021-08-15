7a2487fa7911b21c06b21deebefe27280e70b233
carlos <carlosasjr2003@hotmail.com>
2021-08-15 18:32:34 -0300

    https force sheme

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -33,9 +33,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-/*        if (config('app.env') == 'production') {
+        if (config('app.env') == 'production') {
             URL::forceScheme('https');
-        }*/
+        }
 
         Schema::defaultStringLength('191');
         Paginator::useBootstrap();
